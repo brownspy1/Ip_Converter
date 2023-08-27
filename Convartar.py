270b4358621d206e3fa50baebb4f5c2527219351
M.Mahadi <motivation1424@gmail.com>
2023-08-27 23:52:07 +0600

    Add img

--- Convartar.py
+++ Convartar.py
@@ -1,35 +1,46 @@
 import socket
 import pyfiglet
 
+
 def main():
     while True:
-        banar = pyfiglet.figlet_format("\tIp Convart")
-        print(banar)
-        number = int(input('1.Domain to Ip\n'
-                           '2.Ip to Domain\n'
-                           '3.exit\n'
-                           'Input A number in reng(1-3):'))
-        if number == 1:
-            domain = input('Enter Domain name: ')
-            ip = socket.gethostbyname(domain)
-            print('Ip For {} : {}'.format(domain,ip))
-        elif number == 2:
-            try:
-                ip = input('Enter Ip Addres: ')
-                domain = socket.gethostbyaddr(ip)
-                print('Domain For {} : {}'.format(ip, domain))
-            except:
-                print(f'Wrong ip {ip} or this ip are not online!')
-                return main()
-        elif number == 3:
-            ask = input('Do you want to really exit from this program Y/N')
-            if ask == 'Y' or ask == 'y':
-                break
-            else:
-                main()
-        else:
-            print('Wrong input!')
+        banner = pyfiglet.figlet_format("\tIP Converter")
+        print(banner)
 
+        print('1. Domain to IP')
+        print('2. IP to Domain')
+        print('3. Exit')
+
+        choice = input('Enter a number (1-3): ')
+
+        if choice == '1':
+            domain_to_ip()
+        elif choice == '2':
+            ip_to_domain()
+        elif choice == '3':
+            ask_exit = input('Do you really want to exit the program? (Y/N): ')
+            if ask_exit.lower() == 'y':
+                break
+        else:
+            print('Invalid input! Please choose a valid option.')
+
+
+def domain_to_ip():
+    domain = input('Enter Domain name: ')
+    try:
+        ip = socket.gethostbyname(domain)
+        print('IP Address for {}: {}'.format(domain, ip))
+    except socket.gaierror:
+        print('Unable to resolve domain:', domain)
+
+
+def ip_to_domain():
+    ip = input('Enter IP Address: ')
+    try:
+        domain = socket.gethostbyaddr(ip)
+        print('Domain for {}: {}'.format(ip, domain[0]))
+    except socket.herror:
+        print('Unable to resolve IP:', ip)
 
 
 if __name__ == '__main__':
